9007590bc1037957c064636547f603e664a50fe6
nosmc <nos031130@gmail.com>
2025-10-05 01:57:22 +0800

    Add copyright and contact info to map files
    Inserted copyright notice, usage restrictions, and contact information at the top of index.html, map.js, and map.css for the Stronghold Hex Map. This clarifies code ownership and usage terms.

--- tga/stronghold/map/3rd/scripts/map.js
+++ tga/stronghold/map/3rd/scripts/map.js
@@ -1,4 +1,14 @@
 /**
+ * Copyright © 2025 nos
+ * All Rights Reserved.
+ * 
+ * 未經授權，禁止使用、複製、修改或分發本代碼。
+ * Unauthorized copying, modification, or distribution is strictly prohibited.
+ * 
+ * Contact: discord: nos1130
+ */
+
+/**
  * Stronghold Hex Map - Main Logic
  * 六邊形地圖主邏輯文件
  * 
